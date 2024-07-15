b568fadbdea3315da58469bd566ec81b22d13d00
Mateus Mariani <mzmariani72@gmail.com>
2024-07-15 17:15:18 -0300

    adding functionalities

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,9 +1,13 @@
-document.addEventListener('DOMcontentloaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('form-sorteador').addEventListener('submit', function () {
+        event.preventDefault();
         let numeroMaximo = document.getElementById('numero-maximo').value;
         numeroMaximo = parseInt(numeroMaximo);
 
         let numeroAleatorio = Math.random() * numeroMaximo;
-        alert(numeroAleatorio);
-    })
-})  
+        numeroAleatorio = Math.floor(numeroAleatorio + 1);
+
+        document.getElementById('resultado-valor').innerText = numeroAleatorio;
+        document.querySelector('.resultado').style.display = 'block';
+    });
+});  
